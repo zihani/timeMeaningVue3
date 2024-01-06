44346523b08c033562cad9e1a9fdaf99faa2cbc7
zihanipad <lzzihan@email.cn>
2024-01-06 19:54:19 +0800

    add

---
+++ src/components/ArticleList/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Feature from '@/components/feature/index.vue'
+import Banner from "@/components/public/Banner/index.vue"
+const features = ref([
+    {id: 1, title: 'Akina', img: 'https://s1.ax1x.com/2020/05/14/YDfRnU.jpg'},
+    {id: 2, title: '使用说明', img: 'https://s1.ax1x.com/2020/05/14/YDf4AJ.jpg'},
+    {id: 3, title: '文章归档', img: 'https://s1.ax1x.com/2020/05/14/YDfT91.jpg'},
+    {id: 4, title: '文章归档', img: 'https://s1.ax1x.com/2020/05/14/YDfT91.jpg'},
+    {id: 5, title: '文章归档', img: 'https://s1.ax1x.com/2020/05/14/YDfT91.jpg'},
+    {id: 6, title: '文章归档', img: 'https://s1.ax1x.com/2020/05/14/YDfT91.jpg'},
+    {id: 7, title: '文章归档', img: 'https://s1.ax1x.com/2020/05/14/YDfT91.jpg'},
+])
+</script>
+<template>
+    <div class="articleList">
+        <Banner></Banner>  
+        <div class="site-content">
+            <!--焦点图-->
+            <div class='articlesList'>
+                <section-title>
+                    <div style="display: flex;align-items: flex-end;">笔记<small-ico></small-ico></div>
+                </section-title>
+                <div>
+                    <div  v-for="item in features" :key="item.title">
+                        <Feature :data="item"></Feature>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="less">
+.articlesList{
+    margin-bottom: 300px;
+    padding-bottom: 300px;
+}
+</style>
---
+++ src/components/feature/index.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { ref } from 'vue'
+const props = defineProps<{data:object}>();
+
+const data = ref(props) 
+const router=useRouter()
+</script>
+<template>
+    <div class="feature">
+        <!-- <router-link :to="`/article/${data.id}`">
+            <div ><span class="foverlay">{{data.title}}</span></div>
+            <img :src="data.img">
+        </router-link> -->
+        <div>
+            <span class="foverlay">{{data.title}}</span>
+        </div>
+        <img :src="data.img">
+    </div> 
+</template>
+<style lang="scss" scoped>
+.feature {
+    margin: 4px;
+    width: 150px;
+    height:100px;
+    float:left;
+    img {
+        height: 100px;
+        width: 150px;
+        object-fit: cover;
+        border-radius: 5px;
+    }
+    & a:hover .foverlay{
+        opacity:1;
+    }
+    .foverlay {
+        position: absolute;
+        z-index: 2;
+        width: 150px;
+        height: 100px;
+        text-align: center;
+        line-height: 150px;
+        background: #BBE2DB;
+        color: white;
+        font-size: 16px;
+        opacity: 0;
+        border-radius: 5px;
+        -moz-transition: opacity .4s ease-out;
+        -o-transition: opacity .4s ease-out;
+        -webkit-transition: opacity .4s ease-out;
+        transition: opacity .4s ease-out;
+    }
+}
+@media (max-width:800px) {
+    .feature {
+        margin: 4px;
+        width: 100%;
+        height:50%;
+        float:left;
+        img {
+            height: 50%;
+            width: 100%;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+        & a:hover .foverlay{
+            opacity:1;
+        }
+        .foverlay {
+            position: absolute;
+            z-index: 2;
+            width: 100%;
+            height: 50%;
+            text-align: center;
+            line-height: 50%;
+            background: #BBE2DB;
+            color: white;
+            font-size: 16px;
+            opacity: 0;
+            border-radius: 5px;
+            -moz-transition: opacity .4s ease-out;
+            -o-transition: opacity .4s ease-out;
+            -webkit-transition: opacity .4s ease-out;
+            transition: opacity .4s ease-out;
+        }
+    }
+}
+</style>
